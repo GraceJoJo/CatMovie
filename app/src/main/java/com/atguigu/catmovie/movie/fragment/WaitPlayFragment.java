14af77dbaa6478071c87a973510b3c77b4076f29
zj593743143 <18673248496@163.com>
2016-12-01 19:30:48 +0800

    测试新特性效果成功，待展示解析的json数据

--- app/src/main/java/com/atguigu/catmovie/movie/fragment/WaitPlayFragment.java
+++ app/src/main/java/com/atguigu/catmovie/movie/fragment/WaitPlayFragment.java
@@ -1,5 +1,7 @@
 package com.atguigu.catmovie.movie.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -7,10 +9,12 @@ import android.widget.Toast;
 
 import com.atguigu.catmovie.R;
 import com.atguigu.catmovie.base.BaseFragment;
+import com.atguigu.catmovie.movie.adapter.StickyExampleAdapter;
 import com.atguigu.catmovie.movie.bean.WaitPlayBean;
 import com.atguigu.catmovie.net.CallBack;
 import com.atguigu.catmovie.net.RequestNet;
 import com.atguigu.catmovie.utils.ConstantsUtils;
+import com.atguigu.catmovie.utils.DataUtil;
 import com.google.gson.Gson;
 
 import java.util.List;
@@ -21,10 +25,12 @@ import java.util.List;
 public class WaitPlayFragment extends BaseFragment {
 
     private TextView textView;
+    private View view;
 
     @Override
     public View initView() {
-        View view = View.inflate(mContext, R.layout.fragment_movie_waitplay, null);
+        view = View.inflate(mContext, R.layout.fragment_movie_waitplay, null);
+        initRecyclerView();
         return view;
     }
     @Override
@@ -33,6 +39,46 @@ public class WaitPlayFragment extends BaseFragment {
 //        rl_loading_common.setVisibility(View.VISIBLE);
 //        rl_error_common.setVisibility(View.GONE);
         getDataFromNet();
+    }
+    private void initRecyclerView() {
+        RecyclerView recycleview = (RecyclerView)view.findViewById(R.id.recycleview);
+        final TextView tv_headview = (TextView)view.findViewById(R.id.tv_headview);
+
+        assert recycleview != null;
+        assert tv_headview != null;
+
+        recycleview.setLayoutManager(new LinearLayoutManager(mContext));
+        recycleview.setAdapter(new StickyExampleAdapter(mContext, DataUtil.getData()));
+        recycleview.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                View stickyInfoView = recyclerView.findChildViewUnder(
+                        tv_headview.getMeasuredWidth() / 2, 5);
+
+                if (stickyInfoView != null && stickyInfoView.getContentDescription() != null) {
+                    tv_headview.setText(String.valueOf(stickyInfoView.getContentDescription()));
+                }
+
+                View transInfoView = recyclerView.findChildViewUnder(
+                        tv_headview.getMeasuredWidth() / 2, tv_headview.getMeasuredHeight() + 1);
+
+                if (transInfoView != null && transInfoView.getTag() != null) {
+                    int transViewStatus = (int) transInfoView.getTag();
+                    int dealtY = transInfoView.getTop() - tv_headview.getMeasuredHeight();
+                    if (transViewStatus == StickyExampleAdapter.HAS_STICKY_VIEW) {
+                        if (transInfoView.getTop() > 0) {
+                            tv_headview.setTranslationY(dealtY);
+                        } else {
+                            tv_headview.setTranslationY(0);
+                        }
+                    } else if (transViewStatus == StickyExampleAdapter.NONE_STICKY_VIEW) {
+                        tv_headview.setTranslationY(0);
+                    }
+                }
+            }
+        });
     }
 
     private void getDataFromNet() {
@@ -46,7 +92,6 @@ public class WaitPlayFragment extends BaseFragment {
 //                        rl_error_common.setVisibility(View.VISIBLE);
 //                        rl_loading_common.setVisibility(View.GONE);
                     }
-
                     @Override
                     public void onSuccess(String response, int id) {
                         switch (id) {
